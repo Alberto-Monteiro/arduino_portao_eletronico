60b08385f4acc81c15bfb4ea4f5acd22c9fc2e14
Alberto Monteiro <alberto.costa.monteiro@gmail.com>
2022-03-23 02:16:19 -0300

    Mudanças na estrutura do portão menor.

--- src/mainPortaoPequeno.cpp
+++ src/mainPortaoPequeno.cpp
@@ -8,53 +8,85 @@
 
 #include "Credenciais.h"
 
-BlynkTimer timer;
+BlynkTimer blynkTimer;
+AsyncWebServer serverOTA(80);
+bool isWiFiConected = false;
+int valuePinBotoeira = LOW;
 
-AsyncWebServer server(80);
+void openTheGate(int pinValue);
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
+  blynkTimer.setInterval(300L, []()
+                         {
+    if (!isWiFiConected) {
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    } });
+
+  pinMode(D1, INPUT);
+  blynkTimer.setInterval(50L, []()
+                         {
+      if (valuePinBotoeira != digitalRead(D1)) {
+        valuePinBotoeira = digitalRead(D1);
+        if (valuePinBotoeira == HIGH)
+          openTheGate(1);
+      } });
 
   pinMode(D2, OUTPUT);
-  digitalWrite(D2, LOW);
+  digitalWrite(D2, HIGH);
 
   Serial.begin(9600);
 
   Blynk.begin(authPortaoPequeno, ssidPortaoPequeno, passPortaoPequeno, domain, port);
 
-  server.begin();
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", "ESP8266 Portao pequeno"); });
+  serverOTA.begin();
+  serverOTA.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+               { request->send(200, "text/plain; charset=UTF-8", "ESP8266 Portão pequeno V1.0.1"); });
 
-  AsyncElegantOTA.begin(&server, otaUser, otaPassword);
+  AsyncElegantOTA.begin(&serverOTA, otaUser, otaPassword);
 }
 
 void loop()
 {
   Blynk.run();
-  timer.run();
+  blynkTimer.run();
   AsyncElegantOTA.loop();
 }
 
 BLYNK_WRITE(V1)
 {
-  digitalWrite(LED_BUILTIN, !param.asInt());
-  digitalWrite(D2, param.asInt());
-  //if (param.asInt() == 0)
-  //{
-  //  digitalWrite(LED_BUILTIN, LOW);
-  //  timer.setTimer(
-  //      500L, []()
-  //      { digitalWrite(LED_BUILTIN, HIGH); },
-  //      1);
-  //}
+  openTheGate(param.asInt());
+  Blynk.virtualWrite(V1, 0);
+}
+
+void openTheGate(int pinValue)
+{
+  if (pinValue == 1)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(D2, LOW);
+    blynkTimer.setTimer(
+        300L, []()
+        { 
+          digitalWrite(LED_BUILTIN, HIGH);
+          digitalWrite(D2, HIGH); },
+        1);
+  }
 }
 
 BLYNK_CONNECTED()
 {
+  isWiFiConected = true;
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("IP local:");
   Serial.println(WiFi.localIP());
   Blynk.virtualWrite(V10, WiFi.localIP().toString().c_str());
+}
+
+BLYNK_DISCONNECTED()
+{
+  isWiFiConected = false;
+  digitalWrite(LED_BUILTIN, LOW);
 }
